03ba434d67cc2235e1733b3322f68a925d0c3022
Igbadumhe <perfectimhetsike.igbadumhe@stanbicibtc.com>
2021-03-17 23:47:28 +0100

    done react typescript todo

---
+++ src/Components/TodoTask.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { ITask } from '../Interface'
+interface Props {
+    task : ITask,
+    completeTask(taskNameToDelete:string):void
+}
+
+const TodoTask =({task, completeTask}:Props) => {
+    return (
+        <div className="task">
+            <div className="content">
+                <span>{task.taskName}<br/></span>
+                <span>{task.deadline}</span>
+            </div>
+            <button onClick={()=> {
+                completeTask(task.taskName)
+            }}>X</button>
+            
+        </div>
+        
+    )
+}
+
+export default TodoTask
